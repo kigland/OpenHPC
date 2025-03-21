f2ec1cfc0dd0aa88d31b2d347525b2f37f53c88b
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-03-21 23:12:19 +0000

    fix

--- tools/cli/main.go
+++ tools/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -16,11 +17,24 @@ func main() {
 	switch strings.ToLower(os.Args[1]) {
 	case "req", "request", "create", "c":
 		handler.Request()
-	case "list", "ls":
+	case "list", "ls", "ps":
 		handler.List()
 	case "env", "e":
 		handler.Env()
 	case "token", "t":
 		handler.Token()
+	default:
+		help()
 	}
+}
+
+func help() {
+	h := `
+Usage hpc [command]:
+  - req|request|create|c: create a new VNode
+  - list|ls|ps          : list all VNodes
+  - env|e      [cid?]   : show environment variables of the VNode
+  - token|t    [cid?]   : show tokens of the VNode
+`
+	fmt.Println(strings.TrimSpace(h))
 }
