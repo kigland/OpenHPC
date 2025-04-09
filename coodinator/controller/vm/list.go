03c5df8867ca1763ce104e51c9d3d03d81669b4e
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-04-10 02:57:24 +0800

    chore: refactor

--- coodinator/controller/vm/list.go
+++ coodinator/controller/vm/list.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/docker/docker/api/types/container"
 	"github.com/gin-gonic/gin"
 	"github.com/kigland/OpenHPC/coodinator/models/apimod"
 	"github.com/kigland/OpenHPC/coodinator/shared"
@@ -23,39 +24,10 @@ func list(c *gin.Context) {
 		}
 		for _, containers := range uidToContainers {
 			for svcTagStr, summary := range containers {
-				svcTag, err := svcTag.Parse(svcTagStr)
-				if err != nil {
-					log.Println(err)
+				item, ok := containInfo(svcTagStr, summary)
+				if !ok {
 					continue
 				}
-				item := apimod.VmListItem{
-					Cid:     summary.ID,
-					SvcTag:  svcTag.String(),
-					Sc:      svcTag.ShortCode(),
-					Status:  summary.Status,
-					Owner:   svcTag.Owner,
-					Project: svcTag.Project,
-				}
-
-				ports := []apimod.VmListItemMount{}
-				for _, port := range summary.Ports {
-					ports = append(ports, apimod.VmListItemMount{
-						Host:      fmt.Sprintf("%s:%d", port.IP, port.PublicPort),
-						Container: fmt.Sprintf(":%d", port.PrivatePort),
-					})
-				}
-				item.Port = ports
-
-				mounts := []apimod.VmListItemMount{}
-				for _, mount := range summary.Mounts {
-					mounts = append(mounts, apimod.VmListItemMount{
-						Host:      mount.Source,
-						Container: mount.Destination,
-						Readonly:  !mount.RW,
-					})
-				}
-				item.Mount = mounts
-
 				lists = append(lists, item)
 			}
 		}
@@ -63,4 +35,40 @@ func list(c *gin.Context) {
 	}
 
 	c.JSON(200, vmlists)
+}
+
+func containInfo(svcTagStr string, summary container.Summary) (apimod.VmListItem, bool) {
+	svcTag, err := svcTag.Parse(svcTagStr)
+	if err != nil {
+		log.Println(err)
+		return apimod.VmListItem{}, false
+	}
+	item := apimod.VmListItem{
+		Cid:     summary.ID,
+		SvcTag:  svcTag.String(),
+		Sc:      svcTag.ShortCode(),
+		Status:  summary.Status,
+		Owner:   svcTag.Owner,
+		Project: svcTag.Project,
+	}
+
+	ports := []apimod.VmListItemMount{}
+	for _, port := range summary.Ports {
+		ports = append(ports, apimod.VmListItemMount{
+			Host:      fmt.Sprintf("%s:%d", port.IP, port.PublicPort),
+			Container: fmt.Sprintf(":%d", port.PrivatePort),
+		})
+	}
+	item.Port = ports
+
+	mounts := []apimod.VmListItemMount{}
+	for _, mount := range summary.Mounts {
+		mounts = append(mounts, apimod.VmListItemMount{
+			Host:      mount.Source,
+			Container: mount.Destination,
+			Readonly:  !mount.RW,
+		})
+	}
+	item.Mount = mounts
+	return item, true
 }
