84367e042abad5c1ed70b652812b155348c99154
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-04-13 03:17:54 +0800

    fix

--- tools/common/container.go
+++ tools/common/container.go
@@ -62,7 +62,7 @@ func CreateContainerCustomRDS(dk *dockerProv.DockerHelper, imageName image.Allow
 		BindPort:    port,
 		BindSSHHost: consts.SSH_BIND_HOST,
 		BindSSHPort: sshPort,
-		Provider:    dockerProv.ProviderPodman,
+		Provider:    LoadProvider(),
 	}.Image(imageName).WithGPU(1).WithAutoRestart()
 
 	img.ContainerName = tag.String()
